7fb29fe7bd246003aacd333f326952ce4f891d62
joe.coolish <joe.coolish@b27ca026-fb0b-11de-b7cb-b1160b2c2156>
2010-01-14 02:15:05 +0000

    Added the Serialization Class and Object Classes, as well as created a Unit Testing Project

---
+++ trunk/cs340project/UnitTesting/SerializerTest.cs
@@ -0,0 +1,147 @@
+﻿using cs340project;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace UnitTesting
+{
+    
+    
+    /// <summary>
+    ///This is a test class for SerializerTest and is intended
+    ///to contain all SerializerTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class SerializerTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for XMLSerializeObject
+        ///</summary>
+        [TestMethod()]
+        public void XMLSerializeObjectTest()
+        {
+            Stream stream = null; // TODO: Initialize to an appropriate value
+            object obj = null; // TODO: Initialize to an appropriate value
+            bool expected = false; // TODO: Initialize to an appropriate value
+            bool actual;
+            actual = Serializer.XMLSerializeObject(stream, obj);
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+
+        /// <summary>
+        ///A test for XMLSerialize
+        ///</summary>
+        public void XMLSerializeTestHelper<T>()
+        {
+            Stream stream = null; // TODO: Initialize to an appropriate value
+            T obj = default(T); // TODO: Initialize to an appropriate value
+            bool expected = false; // TODO: Initialize to an appropriate value
+            bool actual;
+            actual = Serializer.XMLSerialize<T>(stream, obj);
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+
+        [TestMethod()]
+        public void XMLSerializeTest()
+        {
+            XMLSerializeTestHelper<GenericParameterHelper>();
+        }
+
+        /// <summary>
+        ///A test for BinarySerializeObject
+        ///</summary>
+        [TestMethod()]
+        public void BinarySerializeObjectTest()
+        {
+            Stream stream = null; // TODO: Initialize to an appropriate value
+            object obj = null; // TODO: Initialize to an appropriate value
+            bool expected = false; // TODO: Initialize to an appropriate value
+            bool actual;
+            actual = Serializer.BinarySerializeObject(stream, obj);
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+
+        /// <summary>
+        ///A test for BinarySerialize
+        ///</summary>
+        public void BinarySerializeTestHelper<T>()
+        {
+            Stream stream = null; // TODO: Initialize to an appropriate value
+            T obj = default(T); // TODO: Initialize to an appropriate value
+            bool expected = false; // TODO: Initialize to an appropriate value
+            bool actual;
+            actual = Serializer.BinarySerialize<T>(stream, obj);
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+
+        [TestMethod()]
+        public void BinarySerializeTest()
+        {
+            BinarySerializeTestHelper<GenericParameterHelper>();
+        }
+
+        /// <summary>
+        ///A test for Serializer Constructor
+        ///</summary>
+        [TestMethod()]
+        public void SerializerConstructorTest()
+        {
+            Serializer target = new Serializer();
+            Assert.Inconclusive("TODO: Implement code to verify target");
+        }
+    }
+}
---
+++ trunk/cs340project/cs340project/Objects.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace cs340project
+{
+    /// <summary>
+    /// Contains Static Serialization Methods for both XML and Binary
+    /// </summary>
+    public class Serializer
+    {
+        public static bool BinarySerializeObject(Stream stream, object obj)
+        {
+            BinaryFormatter bformatter = new BinaryFormatter();
+
+            try
+            {
+                bformatter.Serialize(stream, obj);
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool BinarySerialize<T>(Stream stream, T obj)
+        {
+            BinaryFormatter bformatter = new BinaryFormatter();
+
+            try
+            {
+                bformatter.Serialize(stream, obj);
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool XMLSerializeObject(Stream stream, object obj)
+        {
+            XmlSerializer x = new XmlSerializer(obj.GetType());
+
+            try
+            {
+                x.Serialize(stream, obj);
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool XMLSerialize<T>(Stream stream, T obj)
+        {
+            XmlSerializer x = new XmlSerializer(typeof(T));
+
+            try
+            {
+                x.Serialize(stream, obj);
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+    /// <summary>
+    /// Person Name Class.  Holds different names for a Person
+    /// </summary>
+    [Serializable()]
+    public class PersonName : ISerializable
+    {
+        /// <summary>
+        /// Gets or sets the first name.
+        /// </summary>
+        /// <value>The first name.</value>
+        public string First { get; set; }
+        /// <summary>
+        /// Gets or sets the last name.
+        /// </summary>
+        /// <value>The last name.</value>
+        public string Last { get; set; }
+        /// <summary>
+        /// Gets or sets the nick name.
+        /// </summary>
+        /// <value>The nick name.</value>
+        public string Nick { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PersonName"/> class.
+        /// </summary>
+        public PersonName()
+        {
+            First = string.Empty;
+            Last = string.Empty;
+            Nick = string.Empty;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PersonName"/> class.
+        /// </summary>
+        /// <param name="info">The info.</param>
+        /// <param name="context">The context.</param>
+        public PersonName(SerializationInfo info, StreamingContext context)
+        {
+            First = (string)info.GetValue("First", typeof(string));
+            Last = (string)info.GetValue("Last", typeof(string));
+            Nick = (string)info.GetValue("Nick", typeof(string));
+        }
+
+        #region ISerializable Members
+
+        /// <summary>
+        /// Populates a <see cref="T:System.Runtime.Serialization.SerializationInfo"/> with the data needed to serialize the target object.
+        /// </summary>
+        /// <param name="info">The <see cref="T:System.Runtime.Serialization.SerializationInfo"/> to populate with data.</param>
+        /// <param name="context">The destination (see <see cref="T:System.Runtime.Serialization.StreamingContext"/>) for this serialization.</param>
+        /// <exception cref="T:System.Security.SecurityException">
+        /// The caller does not have the required permission.
+        /// </exception>
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("First", First);
+            info.AddValue("Last", Last);
+            info.AddValue("Nick", Nick);
+        }
+
+        #endregion
+    }
+
+    /// <summary>
+    /// A Person.  Serializable Data
+    /// </summary>
+    [Serializable()]
+    public class Person : ISerializable
+    {
+        /// <summary>
+        /// Gets or sets my name.
+        /// </summary>
+        /// <value>My name.</value>
+        public PersonName MyName { get; set; }
+        /// <summary>
+        /// Gets or sets the age.
+        /// </summary>
+        /// <value>The age.</value>
+        public uint Age { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Person"/> class.
+        /// </summary>
+        public Person()
+        {
+            MyName = new PersonName();
+            Age = 0;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Person"/> class.
+        /// </summary>
+        /// <param name="info">The info.</param>
+        /// <param name="context">The context.</param>
+        public Person(SerializationInfo info, StreamingContext context)
+        {
+            MyName = info.GetValue("MyName", typeof(PersonName)) as PersonName;
+            Age = (uint)info.GetValue("Age", typeof(uint));
+        }
+
+        #region ISerializable Members
+
+        /// <summary>
+        /// Populates a <see cref="T:System.Runtime.Serialization.SerializationInfo"/> with the data needed to serialize the target object.
+        /// </summary>
+        /// <param name="info">The <see cref="T:System.Runtime.Serialization.SerializationInfo"/> to populate with data.</param>
+        /// <param name="context">The destination (see <see cref="T:System.Runtime.Serialization.StreamingContext"/>) for this serialization.</param>
+        /// <exception cref="T:System.Security.SecurityException">
+        /// The caller does not have the required permission.
+        /// </exception>
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("MyName", MyName);
+            info.AddValue("Age", Age);
+        }
+
+        #endregion
+    }
+}
