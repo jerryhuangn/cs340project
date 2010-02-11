da2122093f561ddcc4612ff2fafe6868c63156bd
joe.coolish <joe.coolish@b27ca026-fb0b-11de-b7cb-b1160b2c2156>
2010-02-11 20:30:03 +0000

    Added NodeInsert alpha code.

--- cs340project/Server/Node.cs
+++ cs340project/Server/Node.cs
@@ -8,10 +8,14 @@ namespace Server
 {
     public enum NodeState
     {
-        Inner, Up, Down, Edge
+        Inner, Up, Down, Edge, Largest
     }
-    public class Node
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class Node
     {
+
         #region Global set of nodes; debugging
 
         public static Dictionary<uint, Node> AllNodes = new Dictionary<uint, Node>();
@@ -55,18 +59,28 @@ namespace Server
 
         #endregion
 
+        #region References to other nodes
+
+        List<Node> Neighbors = new List<Node>();
+        Dictionary<uint, Node> Up = new Dictionary<uint, Node>();
+        Dictionary<uint, Node> Down = new Dictionary<uint, Node>();
+        Node Fold { get; set; }
+        Node OldFold { get; set; }
+
+        #endregion
+
         /// <summary>
         /// Gets the node's parent Id.
         /// </summary>
         /// <value>The parent's Id.</value>
-        public int ParentId
+        public uint ParentId
         {
             get
             {
-                int par = 0;
-                for (int i = 1; i < Id; )
+                uint par = 0;
+                for (uint i = 1; i < Id; )
                 {
-                    par = (int)Id - i;
+                    par = Id - i;
                     i *= 2;
                 }
                 return par < 0 ? 0 : par;
@@ -74,10 +88,93 @@ namespace Server
         }
 
         /// <summary>
+        /// Gets the node's Child Id. Purely Logical, node may not exist
+        /// </summary>
+        /// <value>The child's Id.</value>
+        public uint ChildId
+        {
+            get
+            {
+                if (Id == 0)
+                    return nextDimensionChild();
+                uint i = 1;
+                while (i < Id)
+                    i *= 2;
+                return i + Id;
+            }
+        }
+
+        /// <summary>
+        /// Gets the next dimension child Id.
+        /// </summary>
+        /// <returns>A child Id for the next dimension</returns>
+        private uint nextDimensionChild()
+        {
+            uint dim = 1;
+            while (Neighbors.Count(n => n.Id == dim) > 0)
+                dim <<= 1;
+            return dim;
+        }
+
+        /// <summary>
+        /// Gets the logical surrogate id.
+        /// </summary>
+        /// <value>The logical surrogate id.</value>
+        public uint SurrogateId
+        {
+            get
+            {
+                uint sur = Id;
+                int count = 0;
+                while (sur > 0)
+                {
+                    sur >>= 1;
+                    count++;
+                }
+                sur = 1;
+                count++;
+                while (count > 0)
+                {
+                    sur <<= 1;
+                }
+
+                return sur + Id;
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether this instance has a child.
+        /// </summary>
+        /// <value><c>true</c> if this instance has a child; otherwise, <c>false</c>.</value>
+        public bool HasChild
+        {
+            get
+            {
+                return Neighbors.Count(n => n.Id == ChildId) == 1;
+            }
+        }
+
+        /// <summary>
         /// Gets or sets the state of the current node.
         /// </summary>
         /// <value>The state of the current.</value>
-        public NodeState CurrentState { get; private set; }
+        public NodeState CurrentState
+        {
+            get
+            {
+                if (Down.Count > 0)
+                    return NodeState.Down;
+                if (Up.Count > 0)
+                    return NodeState.Up;
+                if (Neighbors.Count(n => n.Id > Id) == 0)
+                    return NodeState.Largest;
+                if (!HasChild)
+                    return NodeState.Edge; 
+               
+                return NodeState.Inner;
+            }
+
+        }
 
         /// <summary>
         /// The binary representation of the node's ID number in the
@@ -101,161 +198,8 @@ namespace Server
         public Node()
         {
             Id = 0;
-            CurrentState = NodeState.Inner;
             Node.AllNodes[Id] = this;
         }
 
-        #region Basic insertion algorithm
-
-        #region static helper methods
-
-        private static bool emptyWeb(Node n)
-        {
-            if (n.Id == 0 && n.Neighbors.Count == 0)
-                return true;
-            return false;
-        }
-
-        private static Node insertionPoint(Node n)
-        {
-            List<Node> right = new List<Node>();
-            right.Add(n);
-            while ((right = goRight(right[0])).Count == 1) ;
-
-            return searchRange(right);
-        }
-
-        private static Node searchRange(List<Node> right)
-        {
-            throw new NotImplementedException();
-        }
-
-        private static List<Node> goRight(Node n)
-        {
-            List<Node> ret = new List<Node>();
-            switch (n.CurrentState)
-            {
-                case NodeState.Up:
-                    break;
-                case NodeState.Down:
-                    break;
-                case NodeState.Edge:
-                    if (perfectCube(n))
-                    {
-                        ret.Add(getNode(0, n));
-                        ret.Add(ret[0]);
-                    }
-                    else
-                    {
-                        ret.Add(n.Fold);
-                    }
-                    break;
-                default:
-
-                    var node = from n1 in n.Neighbors
-                               orderby n1.Id ascending
-                               select n1;
-
-                    ret.Add(node.Last());
-                    break;
-            }
-            return ret;
-        }
-
-        private static Node getNode(uint p, Node n)
-        {
-            switch (n.CurrentState)
-            {
-                case NodeState.Down:
-                    if (n.Down.ContainsKey(p))
-                        return n.Down[p];
-                    break;
-                case NodeState.Up:
-                    if (n.Up.ContainsKey(p))
-                        return n.Up[p];
-                    break;
-            }
-            var nearest = (from node in n.Neighbors
-                           orderby node.Id & p ascending
-                           select node).Last();
-            if (nearest.Id == p)
-                return nearest;
-
-            return getNode(p, nearest);
-        }
-
-        private static bool perfectCube(Node n)
-        {
-            Node neighbor = (from node in n.Neighbors
-                             orderby node.Id ascending
-                             select node).Last();
-
-            if (neighbor.CurrentState == NodeState.Up ||
-                neighbor.CurrentState == NodeState.Down)
-                return false;
-
-            if (n.Id > neighbor.Id)
-                return true;
-
-            return perfectCube(neighbor);
-        }
-
-        #endregion
-
-        #region References to other nodes
-
-        List<Node> Neighbors = new List<Node>();
-        Dictionary<uint, Node> Up = new Dictionary<uint, Node>();
-        Dictionary<uint, Node> Down = new Dictionary<uint, Node>();
-        Node Fold { get; set; }
-        Node OldFold { get; set; }
-
-        #endregion
-
-        /// <summary>
-        /// Inserts a new node in the hyperweb, and returns it.
-        /// If this is the insertion point, insert it here, otherwise
-        /// send it closer to the insertion point.
-        /// </summary>
-        /// <returns>The newly inserted node.</returns>
-        public Node CreateNode()
-        {
-            if (emptyWeb(this))
-            {
-                Node n = new Node(1);
-
-                n.Neighbors.Add(this);
-                Neighbors.Add(n);
-                this.Fold = n;
-                n.Fold = n;
-
-                return n;
-            }
-
-            var insert = insertionPoint(this);
-
-            return insert.InsertChildNode();
-        }
-
-        private Node InsertChildNode()
-        {
-            uint i = 1;
-            while (i < Id)
-                i *= 2;
-            uint childId = i + Id;
-            
-            Node n = new Node(childId);
-
-            foreach (var upNode in Up)
-            {
-                Up.Remove(upNode.Key);
-                upNode.Value.Down.Remove(childId);
-
-            }
-
-            return n;
-        }
-
-        #endregion
     }
 }
